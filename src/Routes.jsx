c72ca912872afae19316bf8192ea2695c100875f
Павел <kurinkov.ps@gmail.com>
2023-11-17 23:56:04 +0300

    change config

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Error404 from './components/pages/404.jsx';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error404 from "./components/pages/404.jsx";
 //import { useAuth } from './hooks/useAuth';
-import { routes } from './dataRoutes.js';
+import { routes } from "./dataRoutes.js";
 
-const App = ()=>{
-   // const {isAuth} = useAuth();
+const App = () => {
+  // const {isAuth} = useAuth();
 
-return (
-    <BrowserRouter>
-        <Routes>
-            {routes.map( route => {
-                return (<Route path={route.path} element={<route.component />} exact={route.exact} key={`route ${route.path}`}/>);
-                
-            })}
-            <Route path="*" element={<Error404 /> }/>
-        </Routes>
+  return (
+    <BrowserRouter basename="/front">
+      <Routes>
+        {routes.map((route) => {
+          return (
+            <Route
+              path={route.path}
+              element={<route.component />}
+              exact={route.exact}
+              key={`route ${route.path}`}
+            />
+          );
+        })}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </BrowserRouter>
-        
-);
+  );
 };
 
 export default App;
